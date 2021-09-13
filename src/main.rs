861ac3e96f1af5536e6de25faa1ad09ce5d3d88c
Isaiah Inuwa <isaiah.inuwa@gmail.com>
2021-09-13 07:29:22 -0500

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,6 @@ use ruma::presence::PresenceState;
 use serde_json::Value;
 use tokio_stream::StreamExt as _;
 
-
 #[tokio::main]
 async fn main() {
     run().await;
@@ -40,7 +39,7 @@ async fn run() {
             .unwrap();
         client
     } else {
-            panic!("No previous session found and no credentials stored in config")
+        panic!("No previous session found and no credentials stored in config")
     };
 
     let filter = FilterDefinition::ignore_all().into();
@@ -65,17 +64,42 @@ async fn run() {
         &PresenceState::Online,
         Some(Duration::from_secs(30)),
     ));
-    let joke_client = hyper::Client::builder().build::<_, hyper::Body>(hyper_tls::HttpsConnector::new());
+    let joke_client =
+        hyper::Client::builder().build::<_, hyper::Body>(hyper_tls::HttpsConnector::new());
     println!("Listening...");
     while let Some(response) = sync_stream.try_next().await.unwrap() {
-        write_state(&State { access_token: client.access_token().expect("logged in client") }).unwrap();
+        write_state(&State {
+            access_token: client.access_token().expect("logged in client"),
+        })
+        .unwrap();
         println!("{}", response.next_batch);
         for (room_id, room_info) in response.rooms.join {
             for e in &room_info.timeline.events {
-                if let AnySyncRoomEvent::Message(AnySyncMessageEvent::RoomMessage(m)) = e.deserialize().unwrap() {
-                        if let MessageType::Text(t) = m.content.msgtype {
-                            println!("{}", t.body);
-                            if t.body.to_ascii_lowercase().contains("joke") {
+                if let AnySyncRoomEvent::Message(AnySyncMessageEvent::RoomMessage(m)) =
+                    e.deserialize().unwrap()
+                {
+                    if let MessageType::Text(t) = m.content.msgtype {
+                        println!("{}:\t{}", m.sender, t.body);
+                        if t.body.to_ascii_lowercase().contains("joke") {
+                            let joke = get_joke(&joke_client.clone()).await.unwrap();
+                            let joke_content = AnyMessageEventContent::RoomMessage(
+                                MessageEventContent::text_plain(joke),
+                            );
+                            let timestamp = SystemTime::now()
+                                .duration_since(SystemTime::UNIX_EPOCH)
+                                .unwrap()
+                                .as_millis()
+                                .to_string();
+                            let req = send_message_event::Request::new(
+                                &room_id,
+                                &timestamp,
+                                &joke_content,
+                            );
+                            client.send_request(req).await.unwrap();
+                        }
+                    }
+                }
+            }
         }
 
         for (room_id, _) in response.rooms.invite {
@@ -104,15 +128,17 @@ async fn run() {
     }
 }
 
-async fn get_joke(client: hyper::Client<HttpsConnector<HttpConnector>>) -> Result<String, Error> {
-    let uri = "https://v2.jokeapi.dev/joke/Programming,Pun,Misc?safe-mode&type=single".parse::<hyper::Uri>().unwrap();
+async fn get_joke(client: &hyper::Client<HttpsConnector<HttpConnector>>) -> Result<String, Error> {
+    let uri = "https://v2.jokeapi.dev/joke/Programming,Pun,Misc?safe-mode&type=single"
+        .parse::<hyper::Uri>()
+        .unwrap();
     let rsp = client.get(uri).await?;
     let bytes = hyper::body::to_bytes(rsp).await?;
     let json = String::from_utf8(bytes.to_vec()).unwrap();
     let joke_obj = serde_json::from_str::<Value>(&json).unwrap();
     let joke = joke_obj["joke"].as_str().unwrap();
     Ok(joke.to_owned())
-}       
+}
 
 struct State {
     access_token: String,
@@ -126,11 +152,7 @@ fn write_state(state: &State) -> Result<(), std::io::Error> {
 
 fn read_state() -> Result<Option<State>, io::Error> {
     match fs::read_to_string("./session") {
-        Ok(access_token) => {
-            Ok(Some(State {
-                access_token,
-            }))
-        },
+        Ok(access_token) => Ok(Some(State { access_token })),
         Err(e) => {
             if let io::ErrorKind::NotFound = e.kind() {
                 Ok(None)
